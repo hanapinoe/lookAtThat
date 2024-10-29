18fc4e212b315fd6484012c5c301b1985ffb94a0
Phuc Phan Cong <phucphancong9210@gmail.com>
2024-10-29 12:43:16 +0700

    Add triton into container

--- app/main.py
+++ app/main.py
@@ -1,15 +1,29 @@
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Form, UploadFile, File, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+import httpx
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+TRITON_SERVER_URL = "http://triton:8000/v2/models/your_model_name/infer"
+
 @app.get("/", response_class=HTMLResponse)
 async def read_form(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
 
-@app.post("/submit/")
-async def handle_form(request: Request, item_id: int = Form(...), q: str = Form(None)):
-    return {"item_id": item_id, "query": q}
+@app.post("/upload")
+async def upload_image(image: UploadFile = File(...)):
+    contents = await image.read()
+
+    # Call Triton Inference Server here
+    async with httpx.AsyncClient() as client:
+        response = await client.post(TRITON_SERVER_URL, files={"image": (image.filename, contents)})
+
+    # Suppose Triton responses a JSON file contains the description about user's image
+    result = response.json()
+    description = result.get("description", "No description")
+
+    return {"description": description}
+    
